03ac2ffaef4e241922d4092f8c80a43f404c6203
Roman <Roman@fgfg.bok>
2023-05-26 14:20:13 +0300

    css,

---
+++ javascripts.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (reg, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+const PORT = 3000
+
+app.listen(3000, () => {
+    console.log('Server started: http://localhost:' + PORT)
+})
