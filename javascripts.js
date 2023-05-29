1a60b29638d8daec67913b7e22074ddad190d727
Roman <Roman@fgfg.bok>
2023-05-29 12:34:27 +0300

    create pages

--- javascripts.js
+++ javascripts.js
@@ -8,6 +8,14 @@ app.get('/', (reg, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/addpage', (reg, res) => {
+    res.sendFile(__dirname + '/addpage.html')
+})
+
+app.get('/calcpage', (reg, res) => {
+    res.sendFile(__dirname + '/calcpage.html')
+})
+
 const PORT = 3000
 
 app.listen(3000, () => {
